613fa0c619fdf7545c64b75cc68120eb0068e291
Pskalou <pascal.padilla@gmail.com>
2022-03-16 14:12:57 +0100

    notebook dichotomie ok

--- cours/algorithmes/algos_1/dichotomie/dichotomie.py
+++ cours/algorithmes/algos_1/dichotomie/dichotomie.py
@@ -4,6 +4,16 @@ auteur : Pascal Padilla
 
 
 def recherche_dichotomique(elem, tab: list) -> int:
+    """Recherche dichotomique dans un tableau trié
+
+    Args:
+        elem (type): élément à rechercher
+        tab (list): tableau dans lequel on cherche
+
+    Returns:
+        int:  * si trouvé     : indice de l'élément
+              * si non trouvé : None
+    """
     g = 0
     d = len(tab) - 1
 
@@ -25,9 +35,33 @@ def recherche_dichotomique(elem, tab: list) -> int:
         # ou bien on poursuit la recherche à droite
         else:
             g = i_median + 1
-        
-        print (g, d)
     
-    return -1
+    return None
 
-print(recherche_dichotomique(12, list(range(10))))
+
+def recherche_brute(elem, tab):
+    for i in range(len(tab)):
+        if tab[i] == elem:
+            return i
+    
+    return None
+
+
+# taille des tableaux (en million)
+elements = [2, 4, 8, 16, 32, 64]
+
+from time import time
+for elem in elements:
+    tab = [0] * (elem * 1_000_000)
+    t0 = time()
+    recherche_dichotomique(elem, tab)
+    duree = time() - t0
+    print(elem, "\t", round(duree, 6))
+
+
+for elem in elements:
+    tab = [0] * (elem * 1_000_000)
+    t0 = time()
+    recherche_brute(elem, tab)
+    duree = time() - t0
+    print(elem, "\t", round(duree, 1))
