82e13fdb5654d25652a7021987ce1b7a210462a1
Pskalou <pascal.padilla@gmail.com>
2022-06-22 19:51:40 +0200

    carte mentale activité snt

---
+++ projets/SNT 202223.mm
@@ -0,0 +1,227 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<node TEXT="SNT 2022/23" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1655123285810">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#f9f9f8" zoom="1.331">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_note_icons="true" associatedTemplateLocation="template:/light_nord_template.mm" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_506805493" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#efefef" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#8fbcbb" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_506805493" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="11" BOLD="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#2e3440" WIDTH="3" DASH="SOLID"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#ffffff" BACKGROUND_COLOR="#2e3440" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true">
+<font SIZE="11" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#f6f9a1" TEXT_ALIGN="LEFT">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#eceff4" BACKGROUND_COLOR="#bf616a" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_915433779" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_915433779"/>
+<font NAME="Ubuntu" SIZE="14"/>
+<edge COLOR="#bf616a"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Séquence 0 - la SNT, c&apos;est quoi ?" POSITION="right" ID="ID_435095535" CREATED="1655126226570" MODIFIED="1655126263119">
+<node TEXT="Séance 1 (brainstorming)" FOLDED="true" ID="ID_658301782" CREATED="1655124815474" MODIFIED="1655126397981">
+<node TEXT="Brainstorming" FOLDED="true" ID="ID_692147565" CREATED="1655124819486" MODIFIED="1655124824058">
+<node TEXT="La SNT, c&apos;est quoi ?" ID="ID_1004857368" CREATED="1655124825022" MODIFIED="1655124830182"/>
+<node TEXT="lien avec l&apos;orientation" FOLDED="true" ID="ID_1678597875" CREATED="1655124831668" MODIFIED="1655124839101">
+<node TEXT="les spécialités" FOLDED="true" ID="ID_1955345036" CREATED="1655124846961" MODIFIED="1655124850580">
+<node TEXT="codage" FOLDED="true" ID="ID_982776469" CREATED="1655124902812" MODIFIED="1655124978252">
+<node TEXT="STI 2D" FOLDED="true" ID="ID_532902043" CREATED="1655124862676" MODIFIED="1655124879624">
+<node TEXT="informatique embarquée" ID="ID_571255914" CREATED="1655124896312" MODIFIED="1655124902557"/>
+</node>
+<node TEXT="NSI" FOLDED="true" ID="ID_847293934" CREATED="1655124854893" MODIFIED="1655124856334">
+<node TEXT="python" ID="ID_473729810" CREATED="1655124979956" MODIFIED="1655124981366"/>
+<node TEXT="html/css" ID="ID_1637635798" CREATED="1655124981675" MODIFIED="1655125007532"/>
+</node>
+<node TEXT="mathématiques" FOLDED="true" ID="ID_1215365893" CREATED="1655124856602" MODIFIED="1655124861610">
+<node TEXT="python" ID="ID_1434981997" CREATED="1655124986864" MODIFIED="1655124988402"/>
+</node>
+<node TEXT="Arts appliqués" FOLDED="true" ID="ID_609835429" CREATED="1655124990054" MODIFIED="1655124995679">
+<node TEXT="html/css" ID="ID_119836201" CREATED="1655124995920" MODIFIED="1655125002598"/>
+<node TEXT="informatique embarquée" ID="ID_1116521700" CREATED="1655125118523" MODIFIED="1655125124078"/>
+</node>
+</node>
+<node TEXT="théorie" FOLDED="true" ID="ID_172896054" CREATED="1655124951429" MODIFIED="1655124956169">
+<node TEXT="mathématiques" ID="ID_1784711150" CREATED="1655124956750" MODIFIED="1655124961168"/>
+<node TEXT="NSI" ID="ID_1026478890" CREATED="1655124961320" MODIFIED="1655124962588"/>
+</node>
+<node TEXT="culture" FOLDED="true" ID="ID_179141512" CREATED="1655125065656" MODIFIED="1655125067554">
+<node TEXT="géopolitique" FOLDED="true" ID="ID_1406214187" CREATED="1655125030358" MODIFIED="1655125035271">
+<node TEXT="réseau sociaux" ID="ID_54503253" CREATED="1655125035548" MODIFIED="1655125039832"/>
+<node TEXT="données structurées" ID="ID_1784546165" CREATED="1655125040006" MODIFIED="1655125044740"/>
+</node>
+<node TEXT="SES" FOLDED="true" ID="ID_470570731" CREATED="1655125046528" MODIFIED="1655125050602">
+<node TEXT="données structurées" ID="ID_627465434" CREATED="1655125040006" MODIFIED="1655125044740"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="décrire les objectifs de l&apos;année en SNT" FOLDED="true" ID="ID_1476900096" CREATED="1655125170689" MODIFIED="1655126069641">
+<node TEXT="progresser dans la maîtrise des outils numérique" ID="ID_1150439899" CREATED="1655125176372" MODIFIED="1655125197449"/>
+<node TEXT="acteur avec les outils numériques" ID="ID_1004666402" CREATED="1655125221200" MODIFIED="1655125232171"/>
+</node>
+</node>
+</node>
+<node TEXT="Séance 2 (données structurées)" ID="ID_1523855722" CREATED="1655127689693" MODIFIED="1655127836880">
+<node TEXT="activité du ticket de caisse" ID="ID_89540056" CREATED="1655127820896" MODIFIED="1655370966091"/>
+<node TEXT="différents formats de fichiers" FOLDED="true" ID="ID_645279918" CREATED="1655127838915" MODIFIED="1655127845545">
+<node TEXT="html" ID="ID_1787193989" CREATED="1655127845916" MODIFIED="1655127847937"/>
+<node TEXT="css" ID="ID_1598874164" CREATED="1655127848102" MODIFIED="1655127849573"/>
+<node TEXT="jpeg" ID="ID_121968251" CREATED="1655127849762" MODIFIED="1655127851233"/>
+<node TEXT="mp4" ID="ID_732186093" CREATED="1655127851413" MODIFIED="1655127852752"/>
+<node TEXT="mp3" ID="ID_636400403" CREATED="1655127852952" MODIFIED="1655127854338"/>
+<node TEXT="csv" ID="ID_1814502386" CREATED="1655127854897" MODIFIED="1655127856602"/>
+<node TEXT="..." ID="ID_722284030" CREATED="1655127856927" MODIFIED="1655127859036"/>
+</node>
+</node>
+<node TEXT="Séance 3 (salle info + code IACA)" FOLDED="true" ID="ID_1271065045" CREATED="1655126201505" MODIFIED="1655126210362">
+<node TEXT="Organiser le numérique en SNT" FOLDED="true" ID="ID_1975117998" CREATED="1655123478397" MODIFIED="1655123761306">
+<node TEXT="Salle informatique" FOLDED="true" ID="ID_1296076587" CREATED="1655123943872" MODIFIED="1655123951295">
+<node TEXT="Préparer une arborescence pour l&apos;année" FOLDED="true" ID="ID_1397085334" CREATED="1655123610901" MODIFIED="1655123719823">
+<node TEXT="Mes Documents/SNT/" ID="ID_571389133" CREATED="1655123723422" MODIFIED="1655123725081"/>
+</node>
+<node TEXT="Outil de gestion de mots de passe" ID="ID_1785000622" CREATED="1655123928190" MODIFIED="1655123934574"/>
+</node>
+</node>
+<node TEXT="Tablette" FOLDED="true" ID="ID_58963590" CREATED="1655124214682" MODIFIED="1655124216426">
+<node TEXT="créer compte GMAIL" ID="ID_1172041733" CREATED="1655126373043" MODIFIED="1655126377163"/>
+<node TEXT="connexion wifi avec la tablette" ID="ID_505578092" CREATED="1655124216737" MODIFIED="1655124227129"/>
+</node>
+</node>
+</node>
+<node TEXT="Séquence 1 - le Web" POSITION="right" ID="ID_1685292074" CREATED="1655126255294" MODIFIED="1655126267574">
+<node TEXT="Séance 4" ID="ID_1052445476" CREATED="1655126219564" MODIFIED="1655126222654">
+<node TEXT="TD créer un site web hors ligne" FOLDED="true" ID="ID_1355161973" CREATED="1655123296488" MODIFIED="1655370703589">
+<node TEXT="créer arborescence" FOLDED="true" ID="ID_704077955" CREATED="1655126417206" MODIFIED="1655126421252">
+<node TEXT="Mes Documents/SNT/Site" FOLDED="true" ID="ID_367882496" CREATED="1655123484647" MODIFIED="1655123698547">
+<node TEXT="index.html" ID="ID_480795866" CREATED="1655123555035" MODIFIED="1655123559551"/>
+<node TEXT="Mes Documents/Site/img" ID="ID_1544374764" CREATED="1655123484647" MODIFIED="1655123541961"/>
+<node TEXT="Mes Documents/Site/css" ID="ID_704815633" CREATED="1655123484647" MODIFIED="1655123549729"/>
+</node>
+</node>
+<node TEXT="distribuer les fichiers" FOLDED="true" ID="ID_244333046" CREATED="1655126410784" MODIFIED="1655126417057">
+<node TEXT="index.html" ID="ID_1094044758" CREATED="1655126462778" MODIFIED="1655126466310"/>
+</node>
+</node>
+<node TEXT="à chaque séance, compléter le cahier de texte personnel en HTML" ID="ID_104241377" CREATED="1655370704887" MODIFIED="1655370721680"/>
+</node>
+</node>
+<node TEXT="Séquence 2 - Informatique Embarquée" FOLDED="true" POSITION="right" ID="ID_1287699642" CREATED="1655126269016" MODIFIED="1655126299166">
+<node TEXT="TD Créer une application téléphone simple niveau 1" FOLDED="true" ID="ID_1606577463" CREATED="1655126331768" MODIFIED="1655370887327">
+<node TEXT="app inventor" ID="ID_1269534429" CREATED="1655370781375" MODIFIED="1655370784762"/>
+<node TEXT="clac des mains" ID="ID_1516960885" CREATED="1655370787154" MODIFIED="1655370790592"/>
+</node>
+<node TEXT="TD Créer une app plus intelligente niveau 2" FOLDED="true" ID="ID_1862720705" CREATED="1655370791352" MODIFIED="1655370891392">
+<node TEXT="secouer téléphone" ID="ID_795476944" CREATED="1655370800523" MODIFIED="1655370804976"/>
+</node>
+<node TEXT="TD Programmer la carte Micro:bit niveau 1" FOLDED="true" ID="ID_1196899565" CREATED="1655370812772" MODIFIED="1655370881739">
+<node TEXT="langage : en bloc" ID="ID_358495811" CREATED="1655370823388" MODIFIED="1655370871752"/>
+<node TEXT="outil en ligne : makecode" ID="ID_1803310572" CREATED="1655370854546" MODIFIED="1655370866144"/>
+<node TEXT="programmation par blocs" ID="ID_227832304" CREATED="1655370826455" MODIFIED="1655370833142"/>
+</node>
+<node TEXT="TD Programmer la carte Micro:bit niveau 2" FOLDED="true" ID="ID_1014005176" CREATED="1655370833909" MODIFIED="1655370846268">
+<node TEXT="langage : en Python" ID="ID_1952167977" CREATED="1655370846609" MODIFIED="1655370875994"/>
+</node>
+<node TEXT="TD créer une interface téléphone pour manipuler la carte Micro:bit" ID="ID_604574682" CREATED="1655370899560" MODIFIED="1655370931478"/>
+</node>
+<node TEXT="Activités &quot;Informatique débranchée&quot; (unplugged)" FOLDED="true" POSITION="right" ID="ID_5891795" CREATED="1655124433224" MODIFIED="1655126323173">
+<node TEXT="Base de données à tricoter" ID="ID_1929533280" CREATED="1655124440294" MODIFIED="1655124445501"/>
+<node TEXT="Graphe d&apos;amis" ID="ID_1377363067" CREATED="1655124445899" MODIFIED="1655124449298"/>
+</node>
+<node TEXT="pédagogie" FOLDED="true" POSITION="left" ID="ID_1114573406" CREATED="1655127999062" MODIFIED="1655128001614">
+<node TEXT="groupes de travail" FOLDED="true" ID="ID_692776333" CREATED="1655128001898" MODIFIED="1655128004892">
+<node TEXT="3/4 élèves" FOLDED="true" ID="ID_1810749135" CREATED="1655128005091" MODIFIED="1655128010231">
+<node TEXT="rôles" FOLDED="true" ID="ID_488558048" CREATED="1655128011005" MODIFIED="1655128014598">
+<node TEXT="à réfléchir" ID="ID_1609691520" CREATED="1655128121642" MODIFIED="1655128124450"/>
+<node TEXT="par exemple" FOLDED="true" ID="ID_1278155578" CREATED="1655128126624" MODIFIED="1655128128762">
+<node TEXT="porte parole" FOLDED="true" ID="ID_1168687821" CREATED="1655128041501" MODIFIED="1655128046693">
+<node TEXT="pose des questions à l&apos;enseignant" ID="ID_578922764" CREATED="1655128081256" MODIFIED="1655128088725"/>
+</node>
+<node TEXT="expert" ID="ID_1709145438" CREATED="1655128046860" MODIFIED="1655128050057"/>
+<node TEXT="rapporteur" FOLDED="true" ID="ID_562853323" CREATED="1655128097914" MODIFIED="1655128100525">
+<node TEXT="présente le travail" ID="ID_511226417" CREATED="1655128103058" MODIFIED="1655128108225"/>
+</node>
+<node TEXT="youtubeur" FOLDED="true" ID="ID_675733831" CREATED="1655128226570" MODIFIED="1655128233087">
+<node TEXT="réalisateur de screencast" ID="ID_1818309471" CREATED="1655128202027" MODIFIED="1655128207718"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="idées projet" POSITION="left" ID="ID_1935051387" CREATED="1655735948625" MODIFIED="1655735953876">
+<node TEXT="robots" ID="ID_243394482" CREATED="1655736323645" MODIFIED="1655736327910">
+<node TEXT="programmation + estétique" ID="ID_527263983" CREATED="1655735954247" MODIFIED="1655736465718">
+<node TEXT="sortir d&apos;un labyrinthe le plus rapidement possible" ID="ID_125310185" CREATED="1655735958051" MODIFIED="1655735973679"/>
+<node TEXT="sortir d&apos;un périmètre délimité par des lignes noires" ID="ID_1068520698" CREATED="1655736047602" MODIFIED="1655736061320"/>
+<node TEXT="rester le dernier robot debout" ID="ID_1615752599" CREATED="1655736077530" MODIFIED="1655736092639"/>
+<node TEXT="robots + balle =&gt; marquer un but" ID="ID_1581031789" CREATED="1655736092823" MODIFIED="1655736121414"/>
+</node>
+<node TEXT="défilé de mode des robots" ID="ID_151300206" CREATED="1655736623962" MODIFIED="1655736633132"/>
+</node>
+<node TEXT="image" ID="ID_1949109478" CREATED="1655736277300" MODIFIED="1655736279101">
+<node TEXT="cacher un texte dans une image (grâce au codage des pixels en triplet RVB : (255, 255, 255)" ID="ID_683485845" CREATED="1655736279358" MODIFIED="1655736310069"/>
+</node>
+</node>
+</node>
+</map>
